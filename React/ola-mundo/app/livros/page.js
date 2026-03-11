60ef66f33acc18a1937a610f51e97dd5430495f0
LuanM280 <luanlima01388@gmail.com>
2026-03-10 22:15:26 -0300

    linkar o banco de dados com o código

---
+++ React/ola-mundo/app/livros/page.js
@@ -0,0 +1,39 @@
+'use client'
+import { createClient } from '@supabase/supabase-js'
+import { useState } from 'react'
+const supabase = createClient('https://hevyslzodechuupmqkoa.supabase.co', 'sb_publishable_ah-tn7rG1NbqoZAjx_mHTg_MEJnz2nd')
+
+function Livros() {
+
+    const [livros, alteraLivros] = useState([])
+
+    async function buscarLivros() {
+        const { data, error } = await supabase
+            .from('livros')
+            .select()
+
+        console.log(data)
+        alteraLivros(data)
+    }
+
+    return (
+        <div>
+            <h1>Livros</h1>
+            <p>Dados dos livros que vieram do Banco</p>
+            <hr />
+
+            <button onClick={buscarLivros} type='button' className='btn btn-primary' > Carregar Livros </button>
+
+            <br/>
+
+            <ul>
+                {
+                    livros.map(
+                        item => <li> Título: {item.nome} escrito por {item.escritor} </li>
+                    )
+                }
+            </ul>
+        </div>
+    );
+}
+export default Livros;
